c82e4ef1148f322bd4e72cc4051db7053b046385
SpikeHD <spikegdofficial@gmail.com>
2023-04-16 20:48:00 -0700

    game and anims and such

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@
 async function loadPage(page) {
   // Set page title to the filename, with captialization.
   const filename = page.split('/').pop().split('.')[0];
-  document.title = `Quarticore | ${filename.charAt(0).toUpperCase() + filename.slice(1)}`;
+  document.title = `QCORE | ${filename.charAt(0).toUpperCase() + filename.slice(1)}`;
 
   const response = await fetch(page);
   const html = await response.text();
   document.querySelector('#content').innerHTML = html;
+}
+
+function openNewTab(url) {
+  window.open(url, '_blank');
 }
